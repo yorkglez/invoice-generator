372f34376ef2ab8e67093a62d64b29b6fb07c6eb
York <jorgeglez.alva94@gmail.com>
2024-10-22 03:07:04 -0600

    config: tailwind config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        '104': '26rem',
+      },
+      backgroundColor:
+        {
+          lightGray: '#f5f5f5',
+          darkBlue: '#192733',
+        },
+      fontSize: {
+        'normal': '14px'
+      },
+      width: {
+        '23': '5.75rem'
+      },
+      height: {
+        '23': '5.75rem'
+      }
+    }
   },
   plugins: [],
 }
