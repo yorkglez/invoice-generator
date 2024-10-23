d8084222499d2dbff05afefa1baf60d0fddd7007
York <jorgeglez.alva94@gmail.com>
2024-10-22 20:32:38 -0600

    feat: add route 'invoice-generator'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {InvoiceComponent} from './components/landing/components/invoice/invoice.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    component: InvoiceComponent,
+    path: 'invoice-generator'
+  },
+  {path: '**', redirectTo: 'invoice-generator'}
+
+];
