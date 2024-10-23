213c0faa5bdb6bd40263860bff0ae25324a269e3
York <jorgeglez.alva94@gmail.com>
2024-10-22 23:42:16 -0600

    update apiService add apiUrl

--- src/app/common/services/api.service.ts
+++ src/app/common/services/api.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'http://localhost:8000/api';
+  private baseUrl = environment.apiURL;
 
   constructor(private http: HttpClient) {
   }
